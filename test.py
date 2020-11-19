abda59e960443e7c01e0c80b85c56a3ed66d5ac5
Chanuk-Yang <ych901208@gmail.com>
2020-11-19 12:31:07 +0900

    revised AP and get_bboxes function

--- test.py
+++ test.py
@@ -35,28 +35,36 @@ class Test:
         self.net = pre_trained_net
         self.net.eval()
         self.loss_total = LossTotal()
-        self.num_TP = 0
+        self.num_TP_set = {}
+        self.num_TP_set_per_predbox = []
         self.num_T = 0
         self.num_P = 0
-        self.precisions = []
-        self.recalls = []
+        self.IOU_threshold = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
+        for iou_threshold in self.IOU_threshold:
+            self.num_TP_set[iou_threshold] = 0
 
     def get_eval_value_onestep(self, lidar_image, camera_image, object_data, plot_bev_image=False):
 
         start = time.time()
         pred_cls, pred_reg = self.net(lidar_image, camera_image)
         inter_1 = time.time()
-        print("inference algorithm time :", inter_1 - start, "s")     
-        pred_bboxes = self.get_bboxes(pred_cls, pred_reg, selet_bbox_threshold=50)
+        print("inference algorithm time :", inter_1 - start, "s")   
+        pred_cls = pred_cls.cpu().clone().detach()
+        pred_reg = pred_reg.cpu().clone().detach()
+
         inter_2 = time.time()
-        # print("get_bboxes time :", inter_2 - inter_1, "s")    
+        print("device to host time :", inter_2 - inter_1, "s")   ## its toooooooo slow
+  
+        pred_bboxes = self.get_bboxes(pred_cls, pred_reg, score_threshold=0.7)
+        inter_3 = time.time()
+        print("get_bboxes time :", inter_3 - inter_2, "s")    
         # refined_bbox = self.NMS_IOU(pred_bboxes, nms_iou_score_theshold=0.01)
         refined_bbox = self.NMS_SAT(pred_bboxes)
-        print("NMS time :", time.time() - inter_2, "s") 
+        print("NMS time :", time.time() - inter_3, "s") 
         print("total algorithm time :", time.time() - start, "s")
         print("=" * 50)
         self.loss_value = self.loss_total(object_data, pred_cls, pred_reg)
-        
+        print(refined_bbox)
 
         if plot_bev_image:
             lidar_image_with_bboxes = putBoundingBox(lidar_image, refined_bbox[0])
@@ -64,7 +72,8 @@ class Test:
         self.precision_recall_singleshot(refined_bbox[0], object_data, ap_iou_score_threshold=0.5) # single batch
         return self.loss_value.item(), pred_cls, pred_reg
     
-    def get_bboxes(self, pred_cls, pred_reg, selet_bbox_threshold=50):
+    def get_bboxes_sort(self, pred_cls, pred_reg, selet_bbox_threshold=50):        
+        
         B, C, W, H = pred_cls.shape
         selected_bboxes_batch =[]
         for b in range(B):
@@ -76,6 +85,28 @@ class Test:
             col = indices % H
             selected_bboxes = pred_reg[b, :7, row[:selet_bbox_threshold], col[:selet_bbox_threshold]]
             selected_bboxes_batch.append(selected_bboxes.permute((1,0)))
+        return selected_bboxes_batch
+    
+    
+    
+    def get_bboxes(self, pred_cls, pred_reg, score_threshold=0.7):
+        """
+        get bounding box score threshold instead of selecting bounding box
+        """
+        B, C, W, H = pred_cls.shape
+        selected_bboxes_batch =[]
+        for b in range(B):
+            # need to change two anchor at pred_cls and pred_reg
+            start = time.time()
+            pred_cls_= pred_cls[b,1].view(-1) > score_threshold
+
+
+            indices = torch.nonzero(pred_cls_).view(-1)
+            print("bbox time: ", time.time() - start)
+
+            pred_reg_ = pred_reg[b, :7, :, :].view((7,-1))
+            selected_bboxes = pred_reg_[:,indices].permute(1,0)
+            selected_bboxes_batch.append(selected_bboxes)
         return selected_bboxes_batch
         
         
@@ -91,14 +122,14 @@ class Test:
                 if len(filtered_bboxes) == 0:
                     filtered_bboxes.append(bbox)
                     continue
-                center = bbox[:3].cpu().clone().detach().numpy()
-                box_size = bbox[3:6].cpu().clone().detach().numpy()
-                heading_angle = bbox[6].cpu().clone().detach().numpy()
+                center = bbox[:3].numpy()
+                box_size = bbox[3:6].numpy()
+                heading_angle = bbox[6].numpy()
                 cand_bbox_corners = get_3d_box(center, box_size, heading_angle)
                 for selected_bbox in filtered_bboxes:
-                    center_ = selected_bbox[:3].cpu().clone().detach().numpy()
-                    box_size_ = selected_bbox[3:6].cpu().clone().detach().numpy()
-                    heading_angle_ = selected_bbox[6].cpu().clone().detach().numpy()
+                    center_ = selected_bbox[:3].numpy()
+                    box_size_ = selected_bbox[3:6].numpy()
+                    heading_angle_ = selected_bbox[6].numpy()
                     selected_bbox_corners = get_3d_box(center_, box_size_, heading_angle_)
                     (IOU_3d, IOU_2d) = box3d_iou(cand_bbox_corners, selected_bbox_corners)
                     if IOU_3d < nms_iou_score_theshold:
@@ -115,19 +146,22 @@ class Test:
         for b in range(B):
             filtered_bboxes = []
             filtered_bboxes_index = []
+            if pred_bboxes[b].shape[0] == 0:
+                filtered_bboxes_batch.append(None)
+                continue
             for i in range(pred_bboxes[b].shape[0]):
                 bbox = pred_bboxes[b][i]
                 if len(filtered_bboxes) == 0:
                     filtered_bboxes.append(bbox)
                     continue
-                center = bbox[:3].cpu().clone().detach().numpy()
-                box_size = bbox[3:6].cpu().clone().detach().numpy()
-                heading_angle = bbox[6].cpu().clone().detach().numpy()
+                center = bbox[:3].numpy()
+                box_size = bbox[3:6].numpy()
+                heading_angle = bbox[6].numpy()
                 cand_bbox_corners = get_vertice_rect(center, box_size, heading_angle)
                 for selected_bbox in filtered_bboxes:
-                    center_ = selected_bbox[:3].cpu().clone().detach().numpy()
-                    box_size_ = selected_bbox[3:6].cpu().clone().detach().numpy()
-                    heading_angle_ = selected_bbox[6].cpu().clone().detach().numpy()
+                    center_ = selected_bbox[:3].numpy()
+                    box_size_ = selected_bbox[3:6].numpy()
+                    heading_angle_ = selected_bbox[6].numpy()
                     selected_bbox_corners = get_vertice_rect(center_, box_size_, heading_angle_)
                     is_overlapped = separating_axis_theorem(cand_bbox_corners, selected_bbox_corners)
                     if is_overlapped:
@@ -139,36 +173,49 @@ class Test:
         
         
     def precision_recall_singleshot(self, pred_bboxes, ref_bboxes, ap_iou_score_threshold=0.5):
-        for pred_bbox in pred_bboxes:
-            self.num_P += 1
-            center = pred_bbox[:3].cpu().clone().detach().numpy()
-            box_size = pred_bbox[3:6].cpu().clone().detach().numpy()
-            heading_angle = pred_bbox[6].cpu().clone().detach().numpy()
-            pred_bbox_corners = get_3d_box(center, box_size, heading_angle)
-            true_positive_cand = []
-            true_positive_cand_score = []
-            for ref_bbox in ref_bboxes:
-                center_ = ref_bbox[:3].cpu().clone().detach().numpy()
-                box_size_ = ref_bbox[3:6].cpu().clone().detach().numpy()
-                heading_angle_ = ref_bbox[6].cpu().clone().detach().numpy()
-                ref_bbox_corners = get_3d_box(center_, box_size_, heading_angle_)
-                (IOU_3d, IOU_2d) = box3d_iou(pred_bbox_corners, ref_bbox_corners)
-                if IOU_3d > ap_iou_score_threshold:
-                    true_positive_cand.append(pred_bbox)
-                    true_positive_cand_score.append(IOU_3d)
-                if len(true_positive_cand_score) > 0:
-                    self.num_TP += 1
-            self.precisions.append(self.num_TP/self.num_P)
-            self.recalls.append(self.num_TP)
+        if pred_bboxes != None:
+            print (pred_bboxes)
+            for pred_bbox in pred_bboxes:
+                self.num_P += 1
+                center = pred_bbox[:3].numpy()
+                box_size = pred_bbox[3:6].numpy()
+                heading_angle = pred_bbox[6].numpy()
+                pred_bbox_corners = get_3d_box(center, box_size, heading_angle)
+                true_positive_cand_score = {}
+                for ref_bbox in ref_bboxes:
+                    center_ = ref_bbox[:3].numpy()
+                    box_size_ = ref_bbox[3:6].numpy()
+                    heading_angle_ = ref_bbox[6].numpy()
+                    ref_bbox_corners = get_3d_box(center_, box_size_, heading_angle_)
+                    (IOU_3d, IOU_2d) = box3d_iou(pred_bbox_corners, ref_bbox_corners)
+                    for iou_threshold in self.IOU_threshold:
+                        if IOU_3d > iou_threshold:
+                            true_positive_cand_score[iou_threshold] = IOU_3d
+                
+                for iou_threshold in self.IOU_threshold:
+                    if iou_threshold in true_positive_cand_score:
+                        self.num_TP_set[iou_threshold] += 1
+                self.num_TP_set_per_predbox.append(self.num_TP_set)
         for ref_bbox_ in ref_bboxes:
             self.num_T += 1
 
         
     def display_average_precision(self, plot_AP_graph=False):
-        total_precision = self.num_TP / self.num_P
-        total_recall = self.num_TP / self.num_T
+        """
+        need to IOU threshold varying 
+        """
+        total_precision = {}
+        total_recall = {}
+        for iou_threshold in self.IOU_threshold:
+            total_precision[iou_threshold] = self.num_TP_set[iou_threshold] / (self.num_P + 0.00001)
+            total_recall[iou_threshold] = self.num_TP_set[iou_threshold] / (self.num_T + 0.00001)
 
-        self.recalls = [x / self.num_T for x in self.recalls ]
+        precisions = {}
+        recalls = {}
+        for num_tp_set in self.num_TP_set_per_predbox:
+            for iou_threshold in self.IOU_threshold:
+                precisions[iou_threshold] = [x / self.num_P for x in num_tp_set.values()]
+                recalls[iou_threshold] = [x / self.num_T for x in num_tp_set.values() ]
 
         if plot_AP_graph:
             fig = plt.figure()
@@ -176,18 +223,16 @@ class Test:
             ax.plot([1,2,3])
             fig.savefig('ap_result/test.png')
 
-        print("Precision: ", total_precision)
-        print("Recall: ", total_recall)
+        print("Total Precision: ", total_precision)
+        print("Total Recall: ", total_recall)
+
 
 
     def initialize_ap(self):
-        self.num_TP = 0
+        self.num_TP_set = {}
         self.num_T = 0
         self.num_P = 0
-        self.precisions = []
-        self.recalls = []
-
-
+        self.num_TP_set_per_predbox = []
 
 if __name__ == '__main__':
     os.environ['CUDA_VISIBLE_DEVICES'] = '2'
